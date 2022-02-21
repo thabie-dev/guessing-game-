fa28f55dae314dc9ecf0d74b065add803406186e
unknown <Staff@APB-VR-01.ad.uj.ac.za>
2022-02-22 00:11:32 +0200

    another commit

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
+
+//const randomElem =document.querySelector(".random");
 let randomNumber = Math.ceil((Math.random()* 100))
 
+//randomNumber.innerHTML = randomNumber
 const number_guess = document.querySelector(".number_guess");
 const guess_btn = document.querySelector(".guess_btn");
 const message = document.querySelector(".message");
 
 function guessBtnClicked(){
-    const guessedNo = Number(number_guess.value);
+    const guessedNo = number(number_guess.value);
      if (guessedNo < randomNumber) {
          message.innerHTML = "Your guess is too low";
 
      } else if (guessedNo> randomNumber) {
          message.innerHTML = "Your guess is too high";
      } else {
-        message.innerHTML= 'correct, the secret number is ${guessedNo}'};
+        message.innerHTML= 'correct, the secret number is ${guessedNo}';
      }
 
      setTimeout (function(){
@@ -20,7 +23,7 @@ function guessBtnClicked(){
 
      }, 2000);
 
-
+    }
 guess_btn.addEventListener('click', guessBtnClicked)
 
      
